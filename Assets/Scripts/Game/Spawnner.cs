5464426353d5178ae3b147f06cfc069fff4ff563
Viral Ghag <80312069+Viral00@users.noreply.github.com>
2023-07-02 22:53:11 +0530

    Initial Assets

---
+++ Assets/Scripts/Game/Spawnner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnner : MonoBehaviour
+{
+    public void CreateEnemiesAroundPoint(int num, Vector3 point, float radius)
+    {
+
+        for (int i = 0; i < num; i++)
+        {
+
+            /* Distance around the circle */
+            var radians = 2 * Mathf.PI / num * i;
+
+            /* Get the vector direction */
+            var vertical = Mathf.Sin(radians);
+            var horizontal = Mathf.Cos(radians);
+
+            var spawnDir = new Vector3(horizontal, 0, vertical);
+
+            /* Get the spawn position */
+            var spawnPos = point + spawnDir * radius; // Radius is just the distance away from the point
+
+            /* Now spawn */
+            //var enemy = Instantiate(enemyPrefab, spawnPos, Quaternion.identity) as GameObject;
+
+            /* Rotate the enemy to face towards player */
+            //enemy.transform.LookAt(point);
+
+            /* Adjust height */
+            //enemy.transform.Translate(new Vector3(0, enemy.transform.localScale.y / 2, 0));
+        }
+    }
+}
